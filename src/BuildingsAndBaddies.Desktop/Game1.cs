a3ef95cadd69ba6133d58c4b38f84fda31e501af
Doug Swisher <big.swish@gmail.com>
2023-02-20 13:24:55 -0600

    Add license

--- src/BuildingsAndBaddies.Desktop/Game1.cs
+++ src/BuildingsAndBaddies.Desktop/Game1.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Doug Swisher. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
 using BuildingsAndBaddies.Desktop.Items;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
